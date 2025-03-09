613194acd61fb187fcbd9e0be6d418aa72211710
Masoud <masoudazadi1995@gmail.com>
2025-03-09 19:20:09 +0330

    fixed finally

--- db/connect.js
+++ db/connect.js
@@ -1,8 +1,23 @@
-const mongoose = require("mongoose")
+const { MongoClient } = require('mongodb')
 
-const connectDB = (user, password, host, port, dbName) => {
-  const url = `mongodb://${user}:${password}@${host}:${port}/${dbName}`
-  return mongoose.connect(url)
+const connectDB = async (mongoURI) => {
+  try {
+    const client = new MongoClient(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    // اتصال به دیتابیس
+    await client.connect()
+    console.log("MongoDB connected")
+
+    // انتخاب دیتابیس
+    const db = client.db(process.env.MONGO_DB)
+    return db
+  } catch (error) {
+    console.error("MongoDB connection error:", error)
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
--- server.js
+++ server.js
@@ -1,64 +1,66 @@
-require("dotenv").config()
+require("dotenv").config() // برای بارگذاری متغیرهای محیطی
 require("express-async-errors")
-const cors = require('cors');
+const cors = require('cors')
 
 const express = require("express")
 const app = express()
-app.use(cors());
+app.use(cors())
 
-// Rest of the packages
-const morgan = require("morgan") //HTTP request logger middleware
+// سایر پکیج‌ها
+const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const fileUpload = require("express-fileupload")
-// Require Database
+// اتصال به دیتابیس
 const connectDB = require("./db/connect")
-// Require Routers
+// روت‌ها
 const authRouter = require("./routes/authRoutes")
 const userRouter = require("./routes/userRoutes")
 const productRouter = require("./routes/productRoutes")
 const reviewRouter = require("./routes/reviewRoutes")
 const orderRouter = require("./routes/orderRoutes")
-// Require Middleware
+// میانه‌افزارها
 const notFoundMiddleware = require("./middleware/not-found")
 const errorHandlerMiddleware = require("./middleware/error-handler")
 
-// Invoke Extra packages
+// استفاده از سایر میانه‌افزارها
 app.use(morgan("tiny"))
 app.use(express.json())
 app.use(cookieParser(process.env.JWT_SECRET))
 app.use(express.static("./public"))
 app.use(fileUpload())
 
-// Home get
+// روت‌های اصلی
 app.get("/", (req, res) => {
   res.send("<h1> E-Commerce API</h1>")
 })
 
-// Testing route
 app.get("/api/v1/", (req, res) => {
-  // console.log(req.cookies)
-  // console.log(req.signedCookies)
   res.send("E-commerce API")
 })
 
-// Invoke Routers
+// روت‌ها
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/products", productRouter)
 app.use("/api/v1/reviews", reviewRouter)
 app.use("/api/v1/orders", orderRouter) 
-// Invoke Middleware
+
+// میانه‌افزارهای خطا
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
+
+// اتصال به دیتابیس و راه‌اندازی سرور
 const start = async () => {
   try {
-    // Connect database
-    await connectDB(process.env.MONGO_USER, process.env.MONGO_PASSWORD, process.env.MONGO_HOST, process.env.MONGO_PORT, process.env.MONGO_DB)
-    app.listen(port, () =>
+    // اتصال به MongoDB Atlas با استفاده از URI موجود در .env
+    const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+    await connectDB(mongoURI)
+
+    app.listen(port, () => {
       console.log(`🚀 Server is listening on port ${port}...`)
-    )
+    })
   } catch (error) {
     console.log(error)
   }
